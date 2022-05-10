16944a4187accf4d8f1b99e673b086fe84799648
Bolo <88955176+BoloVEVO@users.noreply.github.com>
2022-05-10 02:38:12 -0500

    HTML5 BUILD CI FIX

--- source/NoteskinHelpers.hx
+++ source/NoteskinHelpers.hx
@@ -87,7 +87,7 @@ class NoteskinHelpers
 
 		// return Paths.getSparrowAtlas('noteskins/' + NoteskinHelpers.getNoteskinByID(FlxG.save.data.noteskin), "shared");
 		#else
-		return BitmapData.fromFile(Std.string(Paths.image('noteskins/Arrows-pixel',("shared")));
+		return BitmapData.fromFile(Std.string(Paths.image('noteskins/Arrows-pixel',"shared")));
 		#end
 	}
 }
